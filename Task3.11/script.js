734a824829d2cb468957604bbf1ac9fa86a0055e
Григорий Кривобоков <danilazaiac@gmail.com>
2024-02-03 10:43:43 +0300

    New Task

---
+++ Task3.11/script.js
@@ -0,0 +1,9 @@
+function button1_Click(){
+    document.getElementById("ImageContent").setAttribute("src", "./examples/flover3.png");
+}
+function button2_Click(){
+    document.getElementById("ImageContent").setAttribute("src", "./examples/Grass1.png");
+}
+function button3_Click(){
+    document.getElementById("ImageContent").setAttribute("src", "./examples/Grass3.png");
+}
